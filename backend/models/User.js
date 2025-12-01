5c45490e0f2fc67f875e0ed1306b133c24b98413
gbultimateservices-collab <gbultimateservices@gmail.com>
2025-12-01 11:04:59 +0530

    Add User model

---
+++ backend/models/User.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  displayName: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    default: 'https://via.placeholder.com/150'
+  },
+  bio: {
+    type: String,
+    default: ''
+  },
+  role: {
+    type: String,
+    enum: ['user', 'streamer', 'admin'],
+    default: 'user'
+  },
+  coins: {
+    type: Number,
+    default: 0
+  },
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  totalGiftsReceived: {
+    type: Number,
+    default: 0
+  },
+  totalGiftsSent: {
+    type: Number,
+    default: 0
+  },
+  isLive: {
+    type: Boolean,
+    default: false
+  },
+  currentStreamId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Stream'
+  }
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
